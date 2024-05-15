2a661fb2762a893c66810c1f2ba59d4dfc82f520
Jangwh <dngus1683@naver.com>
2024-05-16 00:17:47 +0900

    4-1 실습 진행 & 설명 주석 추가

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -53,7 +53,8 @@ void SoftRenderer::LoadScene2D()
 }
 
 // 게임 로직과 렌더링 로직이 공유하는 변수
-Vector2 currentPosition(100.f, 100.f);
+Vector2 CurrentPosition;	// 하트 중심점 위치
+float CurrentScale = 10.f;	// 하트의 크기 배율
 
 // 게임 로직을 담당하는 함수
 void SoftRenderer::Update2D(float InDeltaSeconds)
@@ -63,13 +64,18 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	const InputManager& input = g.GetInputManager();
 
 	// 게임 로직의 로컬 변수
-	static float moveSpeed = 100.f;
+	static float MoveSpeed = 100.f;
+	static float ScaleMin = 5.f;
+	static float ScaleMax = 20.f;
+	static float ScaleSpeed = 20.f;
 
-	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();	// 만약, X축과 Y축 모두 눌렀다면, (1, 1) -> sqrt(2) 만큼의 속도로 움직이며, 이는 1보다 크기에 움직임에 통일성을 위해 정규화한다.
-	Vector2 deltaPosition = inputVector * moveSpeed * InDeltaSeconds;
+	Vector2 InputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();
+	Vector2 DeltaPosition = InputVector * MoveSpeed * InDeltaSeconds;
+	float DeltaScale = input.GetAxis(InputAxis::ZAxis) * ScaleSpeed * InDeltaSeconds;
 
 	// 물체의 최종 상태 설정
-	currentPosition += deltaPosition;
+	CurrentPosition += DeltaPosition;
+	CurrentScale = Math::Clamp(CurrentScale + DeltaScale, ScaleMin, ScaleMax);	// Clamp 함수는, 주어진 변수가 지정해준 최솟값과 최댓값을 넘기지 못하도록 집어주는 함수다.
 }
 
 // 렌더링 로직을 담당하는 함수
@@ -83,31 +89,33 @@ void SoftRenderer::Render2D()
 	DrawGizmo2D();
 
 	// 렌더링 로직의 로컬 변수
-	static const float radius = 50.f;	// 반지름 설정
-	static std::vector<Vector2> Circles;	// 원을 구성하는 모든 점을 담을 vector 초기화
+	float rad = 0.f;
+	static float increment = 0.001f;
+	static std::vector<Vector2> hearts;
 
-	// 원에 해당하는 점들 선별
-	// 앞서 설정한 반지름을 갖는 원이, 외접하는 정사각형 내의 모든 점들을 확인. -> 원점으로부터 해당 점까지의 길이가 반지름보다 작거나 같다면 원의 구성요소로 포함.
-	if (Circles.empty());
-	for (float x = -radius; x <= radius; ++x)
+	// 하트를 구성하는 점 생성
+	if (hearts.empty())
 	{
-		for (float y = -radius; y <= radius; ++y)
+		for (rad = 0.f; rad < Math::TwoPI; rad += increment)	// 0도에서 한 바퀴 돌아 360도까지 계산.
 		{
-			Vector2 TmpVector(x, y);
-			if (TmpVector.SizeSquared() <= radius * radius)	// 제곱값으로 비교를 한다면, 루트를 씌우는 계산을 하지않기 때문에 시간적으로 경제적이다.
-			{
-				Circles.push_back(TmpVector);
-			}
-
+			// 하트 방정식
+			// x와 y를 구하기.
+			float sin = sinf(rad);
+			float cos[4] = { cosf(rad), cosf(2*rad), cosf(3 * rad) , cosf(4 * rad) };
+			float x = 16 * sin * sin * sin;	// 하트 방정식 x 식
+			float y = 13 * cos[0] - 5 * cos[1] - 2 * cos[2] - cos[3]; // 하트 방정식 y식
+			hearts.push_back(Vector2(x, y));	// 위 식을 통해 도출된 좌표값을 하트 컨테이너에 추가.
 		}
 	}
 
-	for (auto const& v : Circles)
+	for (auto const& v : hearts)
 	{
-		r.DrawPoint(v + currentPosition, LinearColor::Blue);	// 원을 구성하는 모든 점 그리기.
+		r.DrawPoint(v * CurrentScale  + CurrentPosition, LinearColor::Blue);	// 위치를 이동시킬 때는 모양을 건드는 것이 아닌, 모든 좌표값들이 동일하게 위치값만 바뀌는 것이기 때문에 더한다.
+																				// scale이나 rotate와 같이 모양과 자세를 건드는 것은 모든 좌표값들에 동일한 값이 계산되지 않고, 선형 변환의 식에 따라 변하기 때문에 곱셈으로 적용한다.
 	}
 
-	r.PushStatisticText("Coordinate : " + currentPosition.ToString());
+	r.PushStatisticText("Position : " + CurrentPosition.ToString());
+	r.PushStatisticText("Scale : " + std::to_string(CurrentScale));
 }
 
 // 메시를 그리는 함수
